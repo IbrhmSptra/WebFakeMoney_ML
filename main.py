5cc5b96d55e70825d017db6dd7d0796579f06bee
IbrhmSptra <sptraibrhm@gmail.com>
2023-06-09 10:51:18 +0700

    Adding small changes in Hasil

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import pickle
 import numpy as np
 import pandas as pd
 import streamlit as st
+import re
 
 #load model KNN from pickle
 with open('modelKNN.pkl','rb') as file:
     modelKNN = pickle.load(file)
 
+#create session dengan default value None untuk menampilkan hasil
 if 'hasil' not in st.session_state:
         st.session_state['hasil'] = None
 
 st.title("Web Pendeteksi Uang Palsu")
-st.write("""Website ini akan mendeteksi uang anda palsu atau tidak, dengan memasukan ciri ciri pada uang anda.
+st.write("""Website ini akan mendeteksi uang anda palsu atau tidak (Dalam Dolar), dengan memasukan ciri ciri pada uang anda.
         Lengkapi data uang anda pada form di bawah dalam ukuran MiliMeter (mm)
          Website ini dapat mendeteksi uang palsu atau asli dengan akurasi 97%. Selamat Mencoba""")
 with st.form("InputanUangUser", clear_on_submit=True) :
@@ -22,8 +24,9 @@ with st.form("InputanUangUser", clear_on_submit=True) :
     height_right = st.number_input("Panjang Kanan", min_value=0)
     margin_low = st.number_input("Margin Bawah",min_value=0)
     margin_up = st.number_input("Margin Atas", min_value=0)
-    submit = st.form_submit_button("submitUang")
+    submit = st.form_submit_button("Submit")
     if submit :
+        #Ubah Ke Dictionary
         data = {
             "diagonal" : diagonal,
             "height_left" : height_left,
@@ -32,9 +35,19 @@ with st.form("InputanUangUser", clear_on_submit=True) :
             "margin_up" : margin_up,
             "length" : length
         }
+        #Ubah ke DataFrame
         data = pd.DataFrame(data, index=[0])
+        #predict inputan dari user
         predict = modelKNN.predict(data)
-        st.session_state['hasil']= predict
-        st.experimental_rerun()
-    if st.session_state['hasil'] != None :
-        st.write("Hasilnya adalah",st.session_state['hasil'])
+        #ubah ke String
+        predict = str(predict)
+        # Clean string ['Asli'] -> Asli
+        hasil = ''.join(re.findall(r'[a-zA-Z]', predict))
+        #masukan ke session beserta kata nya
+        st.session_state['hasil']= "Hasilnya Adalah : "+hasil
+    #Seleksi jika asli tampilkan succses
+    if st.session_state['hasil'] != None and st.session_state['hasil'] != "Hasilnya Adalah : Palsu":
+        st.success(st.session_state['hasil'])
+    #seleksi jika palsu tampilkan Erorr
+    if st.session_state['hasil'] != None and st.session_state['hasil'] != "Hasilnya Adalah : Asli":
+        st.error(st.session_state['hasil'])
